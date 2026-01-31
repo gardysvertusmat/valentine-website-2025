7b8c0bf2f9b88f6208e5fd2412f84cea9f948ded
gardysvertusmat <gardysvertusmat@gmail.com>
2026-01-31 10:20:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "", // Music streaming URL
+        musicUrl:"https://open.spotify.com/track/0iGkRoudiByXaDvuakB18Q?si=v9JXVz_fQpuQ4tRmAnj9Ng,"// Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
